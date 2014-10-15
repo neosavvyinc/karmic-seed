b801b17796546d01e074cc70e049eea9f5f37f1f
unicodesnowman <utfsnowman@gmail.com>
2014-10-15 14:50:42 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+var path = require('path');
+
+module.exports = function (grunt) {
+    require('load-grunt-config')(grunt, {
+        configPath: path.join(process.cwd(), 'tasks')
+    });
+
+    grunt.registerTask('default', ['clean:target', 'concat:dev']);
+};
---
+++ tasks/concat.js
@@ -0,0 +1,13 @@
+module.exports = {
+    options: {
+        sourceMap: true,
+        sourceMapStyle: 'inline'
+    },
+    dev: {
+        src: ['src/main/**/*.js'],
+        dest: 'target/app.js'
+    },
+    prod: {
+
+    }
+}
