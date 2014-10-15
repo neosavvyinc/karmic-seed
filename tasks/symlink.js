f77f44c81832a78aae2f3a46f1bcf4e03bbccbf2
unicodesnowman <utfsnowman@gmail.com>
2014-10-15 17:36:30 -0400

    Add a TODO comment

--- tasks/symlink.js
+++ tasks/symlink.js
@@ -1,5 +1,11 @@
 module.exports = {
     core: {
+        // TODO 
+        // likely rework this to symlink full core directory to web or mobile
+        // so that core template files and other assets are available.
+        // 
+        // an alternative I like is to use the grunt-angular-templates plugin 
+        // to pre-add templates to the $templateCache
         files: [
             {
                 src: 'target/core/core.concat.js',
