4ad25aee090e540c181916301502d4a537eb8de3
Adam Parrish <aparrish@neosavvy.com>
2014-10-21 12:03:33 -0400

    Adding connect.

---
+++ tasks/connect.js
@@ -0,0 +1,10 @@
+module.exports = {
+    server: {
+        options: {
+            port: 9001,
+            base: 'target/web',
+            keepalive: true,
+            open: true
+        }
+    }
+}
